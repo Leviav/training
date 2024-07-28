58206cad60ebe3534e0658b1d0d0c507b41ab280
leviav <arteaga.levipt@gmail.com>
2024-07-28 19:39:07 +0100

    cambios para presentacion/solo index y login

---
+++ pages/login.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="es-ES">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <meta name="author" content="NextGenerationDev">
+   <title>Registro/Login</title>
+   <!--Favicon img-->
+   <link rel="shortcut icon" href="../assets/img/favicon/favicon.png">
+   <!--Bootstarp min css-->
+   <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+   <!--Bootstarp map css-->
+   <link rel="stylesheet" href="../assets/css/bootstrap.css.map">
+   <!--Odometer css-->
+   <link rel="stylesheet" href="../assets/css/odometer.css">
+   <!--Owl Carousel css-->
+   <link rel="stylesheet" href="../assets/css/owl.min.css">
+   <!--Owl Carousel Theme css-->
+   <link rel="stylesheet" href="../assets/css/owl.theme.default.min.css">
+   <!--All min css-->
+   <link rel="stylesheet" href="../assets/css/all.min.css">
+   <!--Animate css-->
+   <link rel="stylesheet" href="../assets/css/animate.css">
+   <!--Popup css-->
+   <link rel="stylesheet" href="../assets/css/magnific-popup.css">
+   <!--Swiper min css-->
+   <link rel="stylesheet" href="../assets/css/swiper-bundle.min.css">
+   <!--Owl Nice select css-->
+   <link rel="stylesheet" href="../assets/css/nice-select.css">
+   <!--main css-->
+   <link rel="stylesheet" href="../assets/css/login_style.css">
+</head>
+<body>
+    <main>
+
+        <div class="contenedor__todo">
+            <div class="caja__trasera">
+                <div class="caja__trasera-login">
+                    <h3>¿Ya tienes una cuenta?</h3>
+                    <p>Inicia sesión para entrar en la página</p>
+                    <button id="btn__iniciar-sesion">Iniciar Sesión</button>
+                </div>
+                <div class="caja__trasera-register">
+                    <h3>¿Aún no tienes una cuenta?</h3>
+                    <p>Regístrate para que puedas iniciar sesión</p>
+                    <button id="btn__registrarse">Regístrarse</button>
+                </div>
+            </div>
+
+            <!--Formulario de Login y registro-->
+            <div class="contenedor__login-register">
+                <!--Login-->
+                <form action="" class="formulario__login">
+                    <h2>Iniciar Sesión</h2>
+                    <input type="text" placeholder="Correo Electronico">
+                    <input type="password" placeholder="Contraseña">
+                    <button>Entrar</button>
+                </form>
+
+                <!--Register-->
+                <form action="" class="formulario__register">
+                    <h2>Regístrarse</h2>
+                    <input type="text" placeholder="Nombre completo">
+                    <input type="text" placeholder="Correo Electronico">
+                    <input type="text" placeholder="Usuario">
+                    <input type="password" placeholder="Contraseña">
+                    <button>Regístrarse</button>
+                </form>
+            </div>
+        </div>
+
+    </main>
+
+
+
+
+
+
+   <!--Jquery 3 6 0 Min Js-->
+   <script src="../assets/js/jquery-3.7.1.min.js"></script>
+   <!--Bootstrap bundle min Js-->
+   <script src="../assets/js/bootstrap.bundle.min.js"></script>
+   <!--Viewport Jquery Js-->
+   <script src="../assets/js/viewport.jquery.js"></script>
+   <!--Wow min Js-->
+   <script src="../assets/js/wow.min.js"></script>
+   <!--Odometer Up min Js-->
+   <script src="../assets/js/odometer.min.js"></script>
+   <!--Owl Carousel min Js-->
+   <script src="../assets/js/owl.min.js"></script>
+   <!--Popup min Js-->
+   <script src="../assets/js/jquery.magnific-popup.min.js"></script>
+   <!--Owl nice Jquery min Js-->
+   <script src="../assets/js/jquery.nice-select.min.js"></script> 
+   <!--Swiper min Js-->
+   <script src="../assets/js/swiper-bundle.min.js"></script> 
+   <!--main Js-->
+   <script src="../assets/js/main.js"></script>
+   <!-- login -->
+   <script src="../assets/js/login.js"></script>
+</body>
+</html>
